99caf9948e1c60a2b23ef413d18fef8e395dac42
aniketsutar63 <sutaraniket63@gmail.com>
2025-11-14 13:27:22 +0530

    program

---
+++ Exception_and_Error.java
@@ -0,0 +1,37 @@
+package Exception_Handling;
+
+public class Exception_and_Error {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Exception => in yours program happy flow  if there are some unexpected event and those are recoverable 
+		 * that is an exception
+		 */
+
+		/*
+		 * Errors => in your program happy flow if  something happens /event happens due to your program terminated
+		 * 
+		 * Errors  are not recoverable 
+		 */
+		
+		/*
+		 * Exception class => Parent of all Exception
+		 */
+		
+		
+		/*
+		 * how to  handel exception 
+		 * 1) Try Block     Try {_}
+		 * 2) catch Block   catch(_)
+		 * 3) throw keyword   Throw
+		 * 4)Finally Block		Finally(_)
+		 */
+		
+	
+	
+	}
+	
+	
+
+}
